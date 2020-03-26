27d401fa1dcb9adf247863bc85c9b0b5762b453e
Sheaxer <sheaxer@gmail.com>
2020-03-26 14:00:36 +0100

    withdrawal day must be Monday through Friday

--- src/main/java/stuba/fei/gono/javablocking/validation/BankingDayValidator.java
+++ src/main/java/stuba/fei/gono/javablocking/validation/BankingDayValidator.java
@@ -5,6 +5,7 @@ import stuba.fei.gono.javablocking.validation.annotations.BankingDay;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Calendar;
 import java.util.Date;
 public class BankingDayValidator implements ConstraintValidator<BankingDay, Date> {
     @Override
@@ -14,6 +15,12 @@ public class BankingDayValidator implements ConstraintValidator<BankingDay, Date
 
     @Override
     public boolean isValid(Date date, ConstraintValidatorContext constraintValidatorContext) {
+
+       Calendar c = Calendar.getInstance();
+       c.setTime(date);
+        int day = c.get(Calendar.DAY_OF_WEEK);
+        if((day == Calendar.SATURDAY) || (day == Calendar.SUNDAY))
+            return false;
         return true;
     }
 }
